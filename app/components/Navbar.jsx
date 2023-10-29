17d7b20e4480cdf783f41cc363305c0b5f1eb6e8
Ekeocha-ibe victor chinemerem <victorekeocha016@gmail.com>
2023-10-29 20:18:04 +0100

    created contact page and updated navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <nav className="w-full  flex lg:flex-row flex-col justify-between items-center lg:px-10 lg:gap-y-0 gap-y-7 fixed top-0 z-50">
-      <div className="lg:w-[50%] w-full justify-between lg:px-0 px-10  flex">
+    <nav className="w-full  flex lg:flex-row flex-col justify-center items-center lg:px-10 lg:gap-y-0 gap-y-7 fixed top-0 z-50">
+      <div className="lg:w-[30%] w-full  justify-between lg:px-0 px-10  flex">
         <Image
           src={"/images/fcscLogo.png"}
           width={1000}
@@ -30,19 +30,30 @@ const Navbar = () => {
         />
       </div>
       <div
-        className={`lg:w-[40%] w-full lg:px-0 px-8 lg:flex ${
+        className={`lg:w-[70%] w-full  lg:px-0 px-8 lg:flex ${
           isOpen ? "flex" : "hidden"
         }`}
       >
-        <ul className="p-6 rounded-lg lg:bg-transparent bg-customBlack  w-full flex justify-around lg:flex-row flex-col text-lg text-white lg:text-customBlack font-medium gap-y-5">
-          <Link href={'/'}>
+        <ul className="p-6 rounded-lg lg:bg-transparent bg-customBlack  w-full lg:h-full flex justify-around lg:items-center lg:flex-row flex-col text-lg text-white lg:text-customBlack font-medium gap-y-5">
+          <Link href={"/"}>
             <li className="cursor-pointer">Home</li>
           </Link>
           <Link href={"/about"}>
             <li className="cursor-pointer">About Us</li>
           </Link>
-          <li className="cursor-pointer">Gallery</li>
+
+          <Link href={"/"}>
+            <li className="cursor-pointer">Gallery</li>
+          </Link>
           <li className="cursor-pointer">Testimonial</li>
+          <div className="lg:w-[20%] w-full h-full  flex lg:justify-center justify-start items-center">
+            <Link href={"/contact"}>
+              <div className="lg:w-[11rem] h-full w-[10rem]  text-white bg-customGreen flex p-5 justify-center items-center gap-x-3 rounded-2xl lg:text-lg text-base font-semibold">
+                <p>Contact Us</p>
+                <Image src={"/plane.svg"} width={20} height={20} alt="plane" />
+              </div>
+            </Link>
+          </div>
         </ul>
       </div>
     </nav>
